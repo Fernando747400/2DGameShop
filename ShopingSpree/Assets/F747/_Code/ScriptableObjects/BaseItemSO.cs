e19e0cc1448086899bf4ac17065f3f8b3511069a
Fernando747 <fernandojcr5@gmail.com>
2024-04-07 16:41:28 -0600

    BaseItems SO Build

--- ShopingSpree/Assets/F747/_Code/ScriptableObjects/BaseItemSO.cs
+++ ShopingSpree/Assets/F747/_Code/ScriptableObjects/BaseItemSO.cs
@@ -1,14 +1,31 @@
+using NaughtyAttributes;
 using System;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Base Item", menuName = "ShoopingSpree/Base Item")]
 public class BaseItemSO : ScriptableObject
 {
     public int ID;
     public string Name;
+    [ShowAssetPreview(256,256)]
     public Sprite Sprite;
     public BodyPartType BodyPartType;
     public int Price;
     public bool Purchased;
+    public bool HasPair;
+    [ShowIf("HasPair")]
+    public BaseItemSO Pair;
+    [ShowIf("HasPair")]
+    public bool BaseOfPair;
+
+    public bool GrantsExtraStats;
+    [ShowIf("GrantsExtraStats")]
+    public int Health;
+    [ShowIf("GrantsExtraStats")]
+    public int Damage;
+    [ShowIf("GrantsExtraStats")]
+    public int Speed;
+
 
     public void LoadInfo()
     {
